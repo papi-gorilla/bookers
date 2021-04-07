718381f2644dd9b2c7766cc15f8c0bfef8e14c57
EC2 Default User <ec2-user@ip-172-31-66-134.ec2.internal>
2021-04-07 18:29:41 +0000

    Add: バリデーション機能及びエラーメッセージ表示機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book=Book.new(book_params)
+    @books=Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice: "メッセージを保存しました"
+    else
+      render :index
+    end
   end
 
   def show
@@ -23,7 +27,7 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book=Book.find(params[:id])
     book.destroy
