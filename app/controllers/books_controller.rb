88afa2ad3824465b4f0d39fbae965f8c2bffcd4f
EC2 Default User <ec2-user@ip-172-31-66-134.ec2.internal>
2021-04-08 05:45:18 +0000

    Add: フラッシュメッセージ及びトップページのレイアウトの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,8 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
     @books=Book.all
     if @book.save
-      redirect_to book_path(@book.id), notice: "メッセージを保存しました"
+      redirect_to book_path(@book.id)
+      flash[:created] = "Book was successfully created."
     else
       render :index
     end
@@ -23,15 +24,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:updated] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
     book=Book.find(params[:id])
     book.destroy
     redirect_to books_path
+    flash[:destroyed] = "Book was successfully destroyed."
   end
 
   private
